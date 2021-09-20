bb0962bdae2a11b782623d5697e966364cc92cf4
Melissa Southern <melissa_southern@hotmail.co.uk>
2021-09-20 12:57:48 +0100

    apart way through creating util function for formatting data

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,5 +1,52 @@
-const seed = (data) => {
+const db = require("../connection");
+const format = require("pg-format");
+
+const seed = async (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
+
+  await db.query(`DROP TABLE IF EXISTS comments;`);
+  await db.query(`DROP TABLE IF EXISTS reviews;`);
+  await db.query(`DROP TABLE IF EXISTS categories;`);
+  await db.query(`DROP TABLE IF EXISTS users;`);
+
+  await db.query(`CREATE TABLE categories (
+    slug VARCHAR(50) PRIMARY KEY NOT NULL,
+    description TEXT NOT NULL
+  );`);
+
+  await db.query(`
+  CREATE TABLE users (
+    username VARCHAR(150) PRIMARY KEY NOT NULL,
+    avatar_url VARCHAR(300) NOT NULL,
+    name VARCHAR(150) NOT NULL
+  );
+`);
+
+  await db.query(`
+  CREATE TABLE reviews (
+    review_id SERIAL PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    review_body VARCHAR(1000) NOT NULL,
+    designer VARCHAR(150) NOT NULL,
+    review_img_url VARCHAR(1000) DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
+    votes INT DEFAULT 0,
+    category VARCHAR(150) REFERENCES categories(slug),
+    owner VARCHAR(150) REFERENCES users(username),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
+`);
+
+  await db.query(`
+  CREATE TABLE comments (
+    comment_id SERIAL PRIMARY KEY,
+    author VARCHAR(150) REFERENCES users(username),
+    review_id INT REFERENCES reviews(review_id),
+    votes INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    body VARCHAR(200) NOT NULL
+  );`);
+
+  console.log(categoryData);
   // 1. create tables
   // 2. insert data
 };
--- db/utils/data-manipulation.js
+++ db/utils/data-manipulation.js
@@ -1,1 +1,8 @@
 // extract any functions you are using to manipulate your data, into this file
+
+exports.formatToNested = (categoryData) => {
+  const formattedCategoryData = data.map((category) => {
+    return [category.slug, category.description];
+  });
+  return formattedCategoryData;
+};
